f1aa9b7afa711409841f2c745553dcce90345933
Hicham <hicham24_1@hotmail.de>
2016-04-18 21:44:28 +0200

    adding config and env

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
+'use strict';
+
 const cluster = require('cluster');
 const http = require('http');
 const numCPUs = require('os').cpus().length;
 
+
 if (cluster.isMaster){
-	var i;
+	let i;
 	for (i=0; i< numCPUs; i++){
 		cluster.fork();
 	}
 
 	cluster.on('exit', (worker, code, signal)=>{
-		console.log(`worker ${worker.process.pid} died`);
+		console.log(`worker ${worker.process.pid} died, spawn new one:`);
+		cluster.fork();
 	}); 
+}else {
 
-}else{
-	
-	http.createServer((req, res) => {
-		res.writeHead(200);
-		res.end('Hello World\n');
-	}).listen(8000);
+  const config = require ('./config/default.js');
+	const express= require('express');
+	const app = express();
 
+	app.get('/', function(req, res){
+		res.send('Hello World!');
+	});
+
+	app.listen(config.port, function(){
+		console.log(`App runnig on Port ${config.port} Worker-ID: ` + cluster.worker.id);
+	});
 }
 
